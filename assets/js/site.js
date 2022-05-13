cf5b19f28d9c649da2819fde5053b4bb5b9866f3
Paul <64870518+woodcock3@users.noreply.github.com>
2022-05-13 19:57:19 +0100

    Delete site.js

--- assets/js/site.js
+++
@@ -1,9 +0,0 @@
-import Alpine from 'alpinejs'
-
-window.Alpine = Alpine
-
-Alpine.start()
-
-import { partytownSnippet } from '@builder.io/partytown/integration';
-
-const snippetText = partytownSnippet();
