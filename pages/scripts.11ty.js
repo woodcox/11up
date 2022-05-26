17adf72aa426d9c0fc91728cfa8dea0e05a8345c
Paul <64870518+woodcock3@users.noreply.github.com>
2022-05-26 13:19:30 +0100

    Update scripts.11ty.js

--- pages/scripts.11ty.js
+++ pages/scripts.11ty.js
@@ -11,12 +11,21 @@ module.exports = class {
 
   async render() {
     await esbuild.build({
-      entryPoints: ['./../assets/js'],
+      entryPoints: ['assets/js'],
       bundle: true,
       minify: isProd,
-      outdir: './dist/assets/js',
+      outdir: './dist/assets/js/esb.js',
       sourcemap: !isProd,
       target: isProd ? 'es6' : 'esnext'
     })
   }
 }
+
+require('esbuild').buildSync({
+  entryPoints: ['app.jsx'],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+  outfile: 'out.js',
+})
