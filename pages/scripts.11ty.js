f0bf9348109924066612ebfaf7ee5a9aded66e57
Paul <64870518+woodcock3@users.noreply.github.com>
2022-05-26 13:20:23 +0100

    Update scripts.11ty.js

--- pages/scripts.11ty.js
+++ pages/scripts.11ty.js
@@ -20,12 +20,3 @@ module.exports = class {
     })
   }
 }
-
-require('esbuild').buildSync({
-  entryPoints: ['app.jsx'],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-  outfile: 'out.js',
-})
