d7235e75a5d0193420c9fcae9a45da988811999f
Paul <64870518+woodcock3@users.noreply.github.com>
2022-05-28 15:32:47 +0100

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,5 @@
 // Email.js version 5
-var tld_ = new Array()
+const tld_ = new Array()
 tld_[0] = "com";
 tld_[1] = "org";
 tld_[2] = "net";
@@ -9,12 +9,12 @@ tld_[10] = "co.uk";
 tld_[11] = "org.uk";
 tld_[12] = "gov.uk";
 tld_[13] = "ac.uk";
-var m_ = "mailto:";
-var a_ = "@";
-var d_ = ".";
+const m_ = "mailto:";
+const a_ = "@";
+const d_ = ".";
 
 function contact1(name, dom, tl, params){
-  var s = e(name,dom,tl);
+  const s = e(name,dom,tl);
   document.write('<a href="'+m_+s+params+'">'+s+'</a>');
 }
 function contact2(name, dom, tl, params, display){
@@ -25,7 +25,7 @@ function contact3(name, dom, tl, params, display){
 }
 
 function e(name, dom, tl){
-  var s = name+a_;
+  const s = name+a_;
   if (tl!=-2){
     s+= dom;
     if (tl>=0)
@@ -36,8 +36,8 @@ function e(name, dom, tl){
   return s;
 }
 function swapper(d){
-  var s = "";
-  for (var i=0; i<d.length; i+=2)
+  const s = "";
+  for (const i=0; i<d.length; i+=2)
     if (i+1==d.length)
       s+= d.charAt(i)
     else
