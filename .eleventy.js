c83959f72290fb985b8c81286cc314317a29a15d
Paul <64870518+woodcock3@users.noreply.github.com>
2022-06-07 16:58:00 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -69,6 +69,7 @@ module.exports = function (eleventyConfig) {
 
   // Let Eleventy transform HTML files as liquidjs
   // So that we can use .html instead of .liquid
+  // 11ty.js template format also picks up on the esbuild.11ty.js script
 
   return {
     markdownTemplateEngine: 'liquid',
