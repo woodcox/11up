cf3b392e5502bdd256cfaedc6a88e6b75b0e870b
Paul <64870518+woodcock3@users.noreply.github.com>
2022-06-11 13:21:21 +0100

    Update icon.js

--- config/shortcodes/icon.js
+++ config/shortcodes/icon.js
@@ -20,7 +20,7 @@ const iconShortcode = (props) => {
     strokeWidth = 2,
     strokeLinecap = 'round',
     strokeLinejoin = 'round',
-    relativeFilePath = listFiles + ".svg"
+    relativeFilePath = listFiles
   } = props ?? {};
   try {
     let svgdata = fs.readFileSync(relativeFilePath);
