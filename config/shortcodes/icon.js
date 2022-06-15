f10690de6dbc03ce3ce76e359b1512eea96c6957
Paul <64870518+woodcock3@users.noreply.github.com>
2022-06-15 19:15:44 +0100

    Update icon.js

--- config/shortcodes/icon.js
+++ config/shortcodes/icon.js
@@ -1,43 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-// let listFiles = []
-// fs.readdirSync(path.resolve('./assets/images/svg/')).forEach(file => {
-//  if (file.split('.').pop() !== 'svg')
-//    return;
-//  listFiles.push(path.resolve('./assets/images/svg/', file))
-//});
-
-
-
-// const getSvgContent = function (file) {
-//   let relativeFilePath = './assets/images/svg/'
-// };
-
-// const svgFile = fs.readdirSync(path.resolve('./assets/images/svg/'));
-
-// let iconShortcode = function (file) {
-//  let svgFilePath = '.assests/images/svg/${file}.svg';
-//  let svgData = fs.readFileSync(svgFilePath,
-//  function(err, contents) {
-//    if (err) return err
-//    return contents
-//  });                             
-                                                                    
-//  return svgData.toString('utf8');
-//};
-
-
-
 iconShortcode = function(file) {  
-  let relativeFilePath = path.resolve(`assets/images/svg/${file}.svg`) 
+  let absFilePath = path.resolve(`assets/images/svg/${file}.svg`) 
 
-  let data = fs.readFileSync(relativeFilePath, function(err, contents) {  
-    if (err) return err  
+  let svgData = fs.readFileSync(absFilePath, function(err, contents) {    
+    if (err) {  
+      throw new Error(err)  
+    }
     return contents  
   });
 
-  return data.toString('utf8')  
+  return svgData.toString('utf8')  
 }
 
 
