50f9d65be8a4b792206ddea33232d4fb2ab71415
Paul <64870518+woodcock3@users.noreply.github.com>
2022-06-16 17:16:53 +0100

    Create feathericons.js

---
+++ config/shortcodes/feathericons.js
@@ -0,0 +1,31 @@
+const feather = require('feather-icons');
+
+const iconShortcode = (props) => {
+  const {
+    icon,
+    className,
+    width = 24,
+    height = 24,
+    stroke = 'currentColor',
+    fill = 'none',
+    strokeWidth = 2,
+    strokeLinecap = 'round',
+    strokeLinejoin = 'round',
+  } = props ?? {};
+  try {
+    return feather.icons[icon].toSvg({
+      class: className,
+      width,
+      height,
+      stroke,
+      fill,
+      'stroke-width': strokeWidth,
+      'stroke-linecap': strokeLinecap,
+      'stroke-linejoin': strokeLinejoin,
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = iconShortcode;
