77ab1cc837bcbf0baaa1b05ea7a7557b62cc677c
Paul <64870518+woodcock3@users.noreply.github.com>
2022-06-21 07:56:40 +0100

    Update feathericons.js

--- config/shortcodes/feathericons.js
+++ config/shortcodes/feathericons.js
@@ -1,4 +1,13 @@
 const feather = require('feather-icons');
+const fs = require("fs");
+
+Object.keys(feather.icons).forEach((icon) => {
+  const svg = feather.icons[icon].toSvg();
+  fs.writeFile(`./_includes/icons/${icon}.svg`, svg, (err) => {
+    if (err) return console.log(err);
+    console.log(`Saved ${icon}.svg`);
+  });
+});
 
 const iconShortcode = (props) => {
   const {
